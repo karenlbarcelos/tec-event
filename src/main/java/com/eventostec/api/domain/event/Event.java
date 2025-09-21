44db87b5f6edc8a809d3c3d43a33a87c92e00530
Karen <karenlbarcelos@hotmail.com>
2025-09-21 16:47:21 -0300

    feat: updated files

---
+++ src/main/java/com/eventostec/api/domain/event/Event.java
@@ -0,0 +1,40 @@
+package com.eventostec.api.domain.event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "event") //representa a tabela
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor //cria um construtor sem argumentos
+@AllArgsConstructor //cria um construtor que recebe todos os argumentos
+public class Event {
+
+    @Id
+    @GeneratedValue  //gera um valor automatico a cada entrada registrada na tabela
+    private UUID id;
+
+    private String title;
+
+    private String description;
+
+    private String imgUrl;
+
+    private String eventUrl;
+
+    private Boolean remote;
+
+    private Date date;
+
+
+}

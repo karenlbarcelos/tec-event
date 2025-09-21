073a1c48947b5e829cd15f63df2752607612c04f
Karen <karenlbarcelos@hotmail.com>
2025-09-21 17:59:00 -0300

    feat: created service class

---
+++ src/main/java/com/eventostec/api/service/EventService.java
@@ -0,0 +1,62 @@
+package com.eventostec.api.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.domain.event.EventRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    public Event createEvent(EventRequestDTO data){
+        String imgUrl = null;
+
+        if(data.image() != null){
+            imgUrl = this.uploadImg(data.image());
+        }
+
+        Event newEvent = new Event();
+        newEvent.setTitle(data.title());
+        newEvent.setDescription(data.description());
+        newEvent.setEventUrl(data.eventUrl());
+        newEvent.setDate(new Date(data.date()));
+        newEvent.setImgUrl(imgUrl);
+
+        return newEvent;
+    }
+
+    private String uploadImg(MultipartFile multipartFile){
+        String imgName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        try {
+            File file = this.convertMultipartToFile(multipartFile);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return "";
+
+    }
+
+    private File convertMultipartToFile(MultipartFile multipartFile) throws IOException {
+
+        File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename())); //garante que nao eh um arquivo null
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+
+        return convFile;
+    }
+}

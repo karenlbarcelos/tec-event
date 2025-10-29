2afbd0a9a7ef9d2bec2a0f0cceb462ca4431d881
Karen <karenlbarcelos@hotmail.com>
2025-10-28 21:12:48 -0300

    feat: implementing new query - search by upcoming event

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -36,7 +36,7 @@ public class EventController {
     @GetMapping //lista os eventos
     public ResponseEntity<List<EventResponseDTO>> getEvents(
             @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
-        List<EventResponseDTO> allEvents = this.eventService.getEvents(page, size);
+        List<EventResponseDTO> allEvents = this.eventService.getUpcomingEvents(page, size);
         return ResponseEntity.ok(allEvents);
     }
 
